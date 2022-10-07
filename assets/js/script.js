aff80d56d99c8be06fc06ab547fca3ffe2574376
Darx <mcrivabode@gmail.com>
2022-10-06 19:47:53 -0700

    clarifying comments for all hour handlers

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 var today = moment().format("MMMM Do, YYYY") //January 1st, 2022
 $("#currentDay").text(today);
 
+//this set of data determines color of each hour's textblock
 var now = parseInt(moment().format("H")); //checks current hour
 var timeBlock = $(".hour") 
 $.each(timeBlock, function() {
@@ -15,66 +16,67 @@ $.each(timeBlock, function() {
     }
 });
 
+//save button functionality: saves hour's schedule data only upon button click
 $(".saveBtn").on("click", function(event) {
     var calendarEvent = event.target.parentElement.previousElementSibling.children[0].value; //saves value of save button's owned calendar event
     localStorage.setItem(event.target.attributes[1].value, calendarEvent); //stores calendar event into local storage 
     console.log(calendarEvent); //checks saved event item
 });
 
+
 $(document).ready(function() {
-    if (localStorage["save-nine"] !== undefined && localStorage["save-nine"] !== null) {
-        var eventNine = $("<p>" + localStorage["save-nine"] + "</p>");
+    if (localStorage["save-nine"] !== undefined) { //if local storage saved an event...
+        var eventNine = $("<p>" + localStorage["save-nine"] + "</p>");//the event is loaded in from local storage
         $("#event-nine").append(eventNine[0].innerText);
-    } else {
-        ("");
+    } else { //if nothing is saved in local storage...
+        ("");//no event is loaded for the corresponding hour
     }
-    if (localStorage["save-ten"] !== undefined && localStorage["save-ten"] !== null) {
+    if (localStorage["save-ten"] !== undefined) { //10 am
         var eventTen = $("<p>" + localStorage["save-ten"] + "</p>");
         $("#event-ten").append(eventTen[0].innerText);
     } else {
         ("");
     }
-    if (localStorage["save-eleven"] !== undefined && localStorage["save-eleven"] !== null) {
+    if (localStorage["save-eleven"] !== undefined) { //11 am
         var eventEleven = $("<p>" + localStorage["save-eleven"] + "</p>");
         $("#event-eleven").append(eventEleven[0].innerText);
     } else {
         ("");
     }
-    if (localStorage["save-twelve"] !== undefined && localStorage["save-twelve"] !== null) {
+    if (localStorage["save-twelve"] !== undefined) { //12 pm
         var eventTwelve = $("<p>" + localStorage["save-nine"] + "</p>");
         $("#event-twelve").append(eventTwelve[0].innerText);
     } else {
         ("");
     }
-    if (localStorage["save-one"] !== undefined && localStorage["save-one"] !== null) {
+    if (localStorage["save-one"] !== undefined) { //1 pm
         var eventOne = $("<p>" + localStorage["save-one"] + "</p>");
         $("#event-one").append(eventOne[0].innerText);
     } else {
         ("");
     }
-    if (localStorage["save-two"] !== undefined && localStorage["save-two"] !== null) {
+    if (localStorage["save-two"] !== undefined) { //2 pm
         var eventTwo = $("<p>" + localStorage["save-two"] + "</p>");
         $("#event-two").append(eventTwo[0].innerText);
     } else {
         ("");
     }
-    if (localStorage["save-three"] !== undefined && localStorage["save-three"] !== null) {
+    if (localStorage["save-three"] !== undefined) { //3 pm
         var eventThree = $("<p>" + localStorage["save-three"] + "</p>");
         $("#event-three").append(eventThree[0].innerText);
     } else {
         ("");
     }
-    if (localStorage["save-four"] !== undefined && localStorage["save-four"] !== null) {
+    if (localStorage["save-four"] !== undefined) { //4 pm
         var eventFour = $("<p>" + localStorage["save-four"] + "</p>");
         $("#event-four").append(eventFour[0].innerText);
     } else {
         ("");
     }
-    if (localStorage["save-five"] !== undefined && localStorage["save-five"] !== null) {
+    if (localStorage["save-five"] !== undefined) { //5 pm
         var eventFive = $("<p>" + localStorage["save-five"] + "</p>");
         $("#event-five").append(eventFive[0].innerText);
     } else {
         ("");
     }
 });
-
